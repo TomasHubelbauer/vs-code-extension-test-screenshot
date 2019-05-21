1e5705d34bb793715e43bc2d16a64bed25c580ed
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-05-21 16:09:55 +0200

    Prototype a working test

--- screenshot/debug.js
+++
@@ -1,1 +0,0 @@
-console.log(process._debugProcess(Number(process.argv[2])));
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,27 +1,65 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
+import ps from 'ps-list';
+import fetch from 'node-fetch';
+import * as ws from 'ws';
+import * as fs from 'fs-extra';
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
+  test("Screenshot", async function () {
+    console.log('Listing running Code instances…');
+    const processes = await ps();
+    const codes = processes.filter(p => p.name === 'Code.exe');
 
-    // Defines a Mocha unit test
-    test("Something 1", function () {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    console.log('Finding main Code instance…');
+    const main = codes.find(code => !codes.find(code2 => code2.pid === code.ppid))!;
+
+    console.log('Attaching to Code instance with PID', main.pid);
+    (process as any)._debugProcess(main.pid);
+
+    console.log('Downloading the debugger information…');
+    const response = await fetch('http://localhost:9229/json');
+    const data = await response.json();
+    const url: string = data[0].webSocketDebuggerUrl;
+
+    console.log('Connecting to the debugger socket…', url);
+    const socket = new ws(url, { perMessageDeflate: false });
+    await new Promise(resolve => socket.once('open', resolve));
+
+    console.log('Subscribing to callbacks…');
+    socket.on('message', async data => {
+      const json = JSON.parse(String(data));
+      switch (json.id) {
+        case 1: {
+          console.log('Evaluating the expression…');
+          // Note that we are using `var` so that we can redeclare the variables on each run making the script reentrant
+          // Note that we are sending a data URI of the image as we cannot send the `NativeImage` instance itself
+          const expression = `
+var electron = process.mainModule.require('electron');
+var webContents = electron.webContents.getAllWebContents()[0] // [1] is the shared process
+new Promise(resolve => webContents.capturePage(image => resolve(image.toDataURL())));
+`;
+          socket.send(JSON.stringify({ id: 2, method: 'Runtime.evaluate', params: { expression, awaitPromise: true } }));
+          break;
+        }
+        case 2: {
+          assert.ok(json.result.result.value);
+          const buffer = Buffer.from(json.result.result.value.substring('data:image/png;base64,'.length), 'base64');
+          console.log('Saving the screenshot buffer…');
+          // Note that `process.cwd()` is in `.vscode-test/vscode-version`
+          await fs.writeFile('../../screenshot.png', buffer);
+          break;
+        }
+        case undefined: {
+          // Ignore events
+          break;
+        }
+        default: {
+          throw new Error(`Unexpected ID.`);
+        }
+      }
     });
 
-    const timeout = 60 * 60 * 1000;
-    test("Screenshot", async function () {
-        await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
-    }).timeout(timeout + 1000);
+    console.log('Enabling the runtime agent…');
+    socket.send(JSON.stringify({ id: 1, method: 'Runtime.enable' }));
+  });
 });
